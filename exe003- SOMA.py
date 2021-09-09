8003857abcb222c266d4b9c47c011ca906116c47
Renan Silva Rocha <renan.silvarocha@bol.com.br>
2021-09-08 21:35:18 -0300

    Correção

--- exe003- SOMA.py
+++ exe003- SOMA.py
@@ -1,4 +1,4 @@
-n1= int (input("Digite um valor: ",))
-n2= int (input("Digite outro valor: ",))
-s= n1+n2
-print('A soma de {} e {} é igual a {}'.format(n1,n2,s))
+n1 = int(input("Digite um valor: ",))
+n2 = int(input("Digite outro valor: ",))
+s = n1+n2
+print('A soma de {} e {} é = {}'.format(n1, n2, s))
